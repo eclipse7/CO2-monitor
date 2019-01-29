f0c27f0ffa221611086c1c04a34878c10b1e4a24
Vlad <khomenko.vlad7@gmail.com>
2019-01-29 22:34:51 +0200

    add button

--- co2_monitor.ino
+++ co2_monitor.ino
@@ -8,9 +8,15 @@
 #define MH_Z19_TX 5
 #define sclk 13
 #define mosi 11
-#define cs 10
-#define rst 9
-#define dc 8
+#define rst 15  // A1 = 15
+#define dc 16  // A2 = 16
+#define cs 17  // A3 = 17
+#define charge_status 18
+#define lcd_power 19
+#define co2_power 3
+#define red_led 9
+#define green_led 10
+#define button 2
 
 #define BLACK 0x0000
 #define BLUE 0x001F
@@ -25,15 +31,23 @@
 #define BROWN 0x3145
 #define PINK 0xF355
 
+#define STATUS_WORK 0
+#define STATUS_WORK_PRESS_KEY 1
+#define STATUS_PW_OFF 2
+#define STATUS_PW_ON 3
+
+#define MAIN_INIT 0
+#define MAIN_HEATING 1
+#define MAIN_WORK 2
+
 // command to ask for data
 byte askco2[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79};
-byte max1k[9] = {0xFF, 0x01, 0x99, 0x00, 0x00, 0x00, 0x03, 0xE8, 0x7B};
 byte max2k[9] = {0xFF, 0x01, 0x99, 0x00, 0x00, 0x00, 0x07, 0xD0, 0x8F};
-byte max3k[9] = {0xFF, 0x01, 0x99, 0x00, 0x00, 0x00, 0x0B, 0xB8, 0xA3};
 byte max5k[9] = {0xFF, 0x01, 0x99, 0x00, 0x00, 0x00, 0x13, 0x88, 0xCB};
 
 SoftwareSerial co2Serial(MH_Z19_RX, MH_Z19_TX); // define MH-Z19
-Adafruit_SSD1331 display = Adafruit_SSD1331(cs, dc, mosi, sclk, rst);
+//Adafruit_SSD1331 display = Adafruit_SSD1331(cs, dc, mosi, sclk, rst);  // software spi
+Adafruit_SSD1331 display = Adafruit_SSD1331(cs, dc, rst);  // hardware spi
 
 int height = 14;
 int width = 0;
@@ -41,64 +55,245 @@ int x_offset = 0;
 int y_offset = 42;
 int prev_digits = 0;
 
+volatile int button_status = 0;
+volatile unsigned long time_hold = 0;
+
 void setup() {
   Serial.begin(115200);
-  delay(500);
-  co2Serial.begin(9600);
-  delay(500);
-  co2Serial.write(max5k, 9);
-  delay(500);
-  co2Serial.write(askco2, 9);
-  Serial.println("First ask to co2Serial");
 
-  // display
-  display.begin(); // инициализация дисплея
-  drawLogo();
-  
-  // loading bar 
-  display.drawRect(0, 60, 96, 4, YELLOW);
-  
-  Serial.println("Heating");
-  // loading bar  
-  int x = 1;
-  for(int i=0; i<47; i++) {
-    display.drawRect(x, 61, 2, 2, YELLOW);
-    x += 2;
-    delay(2553);
-  }
-  Serial.println("GO!");
-  readCO2();
-  readCO2();
+  pinMode(red_led, OUTPUT);
+  pinMode(green_led, OUTPUT);
+  pinMode(co2_power, OUTPUT);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(A5, OUTPUT);
 
-  drawStatic();
+  digitalWrite(red_led, LOW);
+  digitalWrite(green_led, HIGH);
+  digitalWrite(co2_power, LOW);
+  digitalWrite(A5, HIGH);
+
+  pinMode(A4, INPUT_PULLUP);
 }
 
-int bat_value;
-void loop() {
-  int ppm = readCO2();
 
-  bat_value += 1;
-  if (bat_value > 11) bat_value = 0;
-  
+
+int status_w = STATUS_WORK;
+int statusMain = MAIN_INIT;
+
+int logo = 0;
+boolean buttonPress = 0;
+int barI = 0;
+int barX = 1;
+unsigned long heatingTimer = 0;
+unsigned long buttonTimer = 0;
+unsigned long updateTimer = 10000;
+unsigned long checkBatTimer = 10000;
+boolean red_blink = 0;
+
+void loop() {
+  switch (status_w) {
+    case STATUS_WORK:
+      if (!digitalRead(button)) {
+        status_w = STATUS_WORK_PRESS_KEY;
+        buttonTimer = millis();
+      }
+      else {
+        switch (statusMain) {
+          case MAIN_INIT:
+            display.begin(); // инициализация дисплея
+            //  drawLogoMops();
+            drawLogoPica();
+            // loading bar
+            display.drawRect(0, 60, 96, 4, YELLOW);
+
+            co2Serial.begin(9600);
+            delay(500);
+            co2Serial.write(max5k, 9);
+            delay(500);
+            co2Serial.write(askco2, 9);
+            Serial.println("First ask to co2Serial");
+
+            barI = 0;
+            barX = 1;
+            heatingTimer = 0;
+            statusMain = MAIN_HEATING;
+            break;
+
+          case MAIN_HEATING:
+            if (buttonPress) {
+              logo += 1;
+              logo %= 2;
+              if (logo == 0) drawLogoPica();
+              if (logo == 1) drawLogoMops();
+              display.drawRect(0, 60, 96, 4, YELLOW);
+              display.drawRect(1, 61, barX, 2, YELLOW);
+              buttonPress = 0;
+            }
+            if ((millis() - heatingTimer) > 2553) {
+              heatingTimer = millis();
+              display.drawRect(barX, 61, 2, 2, YELLOW);
+              barX += 2;
+              barI += 1;
+
+              if (barI >= 47) {
+                Serial.println("GO!");
+                readCO2();
+                readCO2();
+                drawStatic();
+                statusMain = MAIN_WORK;
+              }
+            }
+            break;
+
+          case MAIN_WORK:
+
+            // main
+            if ((millis() - updateTimer) > 5000) {
+              readAndUpdate();
+              updateTimer = millis();
+            }
+            if ((millis() - checkBatTimer) > 1000) {
+              checkBatAndCharge();
+              if (red_blink) {
+                digitalWrite(red_led, !digitalRead(red_led));
+              }
+              checkBatTimer = millis();
+            }
+
+            break;
+        }
+      }
+      break;
+
+    case STATUS_WORK_PRESS_KEY:
+      if (!digitalRead(button)) {
+        if ((millis() - buttonTimer) > 500) {
+          // do long press
+          status_w = STATUS_PW_OFF;
+        }
+      }
+      else {
+        if ((millis() - buttonTimer) > 30) {
+          // do short press
+          buttonPress = 1;
+          status_w = STATUS_WORK;
+        }
+        else {
+          status_w = STATUS_WORK;
+        }
+        buttonTimer = millis();
+      }
+      break;
+
+    case STATUS_PW_OFF:
+      while (!digitalRead(button)) delay(10);
+      Serial.print("poweroff");
+
+      statusMain = MAIN_INIT;
+      logo = 0;
+      digitalWrite(red_led, LOW);
+      digitalWrite(green_led, LOW);
+      digitalWrite(co2_power, HIGH);
+      digitalWrite(A5, LOW);
+      display.fillScreen(BLACK);
+
+      // sleep attach interrupt
+      while (digitalRead(button)) delay(10);
+      Serial.print("poweron");
+      buttonTimer = millis();
+      status_w = STATUS_PW_ON;
+      break;
+
+    case STATUS_PW_ON:
+      if (!digitalRead(button)) {
+        if ((millis() - buttonTimer) > 500) {
+          while (!digitalRead(button)) delay(10);
+
+          digitalWrite(co2_power, LOW);
+
+          pinMode(A5, OUTPUT);
+          digitalWrite(A5, HIGH);
+
+          status_w = STATUS_WORK;
+        }
+      }
+      else {
+        status_w = STATUS_PW_OFF;
+        buttonTimer = millis();
+      }
+      break;
+  }
+  delay(20);
+}
+
+void readAndUpdate() {
+  int ppm = readCO2();
   reDrawValue(ppm);
   reDrawBar(ppm);
-  fillBatteryIcon(bat_value);
-  drawChargeIcon(bat_value%2);
-    
+
+  red_blink = 0;
+  if (ppm < 800) {
+    digitalWrite(red_led, LOW);
+    digitalWrite(green_led, HIGH);
+  } else {
+    if (ppm < 1200) {
+      digitalWrite(red_led, HIGH);
+      digitalWrite(green_led, HIGH);
+    } else {
+      if (ppm < 2000) {
+        digitalWrite(red_led, HIGH);
+        digitalWrite(green_led, LOW);
+      }
+      else {
+        digitalWrite(green_led, LOW);
+        red_blink = 1;
+      }
+    }
+  }
   Serial.println("===================================================");
   Serial.print("CO2: ");
   Serial.print(ppm);
   Serial.println(" ppm");
-  delay(5000);
 }
 
-void drawLogo(void) {
+void checkBatAndCharge() {
+  int val = analogRead(A4);
+  if (val < 200) drawChargeIcon(1); // val < 181 - charging,  val = 393 - no charging, val > 450 - charging stop
+  else drawChargeIcon(0);
+
+  int bat_value = int((readVcc() - 3200) / 800.0 * 11.0);
+  fillBatteryIcon(bat_value);
+}
+
+long readVcc() {
+  long result; // Read 1.1V reference against AVcc
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA, ADSC));
+  result = ADCL;
+  result |= ADCH << 8;
+  result = 1126400L / result; // Back-calculate AVcc in mV
+  return result;
+}
+
+void drawLogoMops(void) {
   display.fillScreen(BLACK);
   display.drawBitmap(16, 4, mops1, 64, 54, BLACK);
   display.drawBitmap(16, 4, mops4, 64, 54, PINK);
   display.drawBitmap(16, 4, mops5, 64, 54, WHITE);
   display.drawBitmap(16, 4, mops2, 64, 54, BROWN);
   display.drawBitmap(16, 4, mops3, 64, 54, YELLOW);
+}
+
+void drawLogoPica(void) {
+  display.fillScreen(BLACK);
+  display.drawBitmap(27, 11, pica1, 44, 37, BLACK);
+  display.drawBitmap(27, 11, pica_yellow, 44, 37, YELLOW);
+  display.drawBitmap(27, 11, pica_white, 44, 37, WHITE);
+  display.drawBitmap(27, 11, pica_red, 44, 37, RED);
+  display.drawBitmap(27, 11, pica_green, 44, 37, GREEN);
+  display.drawBitmap(27, 11, pica_gray, 44, 37, GRAY);
 }
 
 void drawStatic(void) {
@@ -116,7 +311,7 @@ void drawStatic(void) {
   // battery
   display.drawRect(80, 0, 13, 7, CYAN);
   display.drawLine(93, 2, 93, 4, CYAN);
-  
+
   // bar
   display.drawRoundRect(13, y_offset, 71, height, 4, GRAY);
   // fill green part
@@ -127,18 +322,18 @@ void drawStatic(void) {
 void reDrawValue(int value) {
   int digits = 3;
   if (value >= 1000) digits = 4;
-  if (prev_digits != digits){
+  if (prev_digits != digits) {
     display.fillRect(10, 15, 75, 15, BLACK);
     prev_digits = digits;
   }
- 
+
   // 'ppm' text
   if (digits == 4) display.setCursor(65, 23);  // for 4 digit ppm
   else display.setCursor(61, 23);  // for 3 digit ppm
   display.setTextColor(YELLOW, BLACK);
   display.setTextSize(1);
   display.print("ppm");
-  
+
   // ppm value
   if (digits == 4) display.setCursor(16, 16);  // for 4 digit
   else display.setCursor(24, 16);  // for 3 digit
@@ -173,7 +368,7 @@ void reDrawBar(int value) {
 }
 
 void drawChargeIcon(int on) {
-  // charge icon 
+  // charge icon
   if (on) {
     display.drawLine(75, 0, 73, 3, YELLOW);
     display.drawLine(73, 3, 76, 3, YELLOW);
@@ -191,7 +386,7 @@ void fillBatteryIcon(int value) {
   if (value > 11) value = 11;
   if (value < 0) value = 0;
   display.fillRect(81, 1, value, 5, CYAN);
-  display.fillRect(81+value, 1, 11-value, 5, BLACK);
+  display.fillRect(81 + value, 1, 11 - value, 5, BLACK);
 }
 
 int readCO2() {
@@ -204,6 +399,16 @@ int readCO2() {
 
   memset(response, 0, 9);
   co2Serial.readBytes(response, 9);
+
+    // print response
+    Serial.println(" ");
+    Serial.println("response");
+    for (int i = 0; i < 9; i++) {
+      int r = (int) response[i];
+      Serial.print(r);
+      Serial.print(" ");
+    }
+    Serial.println(" ");
 
   if (response[0] != 0xFF) {
     Serial.print("\n\rWrong starting byte from co2 sensor!");
